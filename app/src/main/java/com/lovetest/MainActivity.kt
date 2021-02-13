fa8477cb0236c1724f2c3ad381f67633b9411af6
zave <zave7@naver.com>
2021-02-13 12:20:40 +0900

    set navigation and kotlin extensions plugins

--- app/src/main/java/com/lovetest/MainActivity.kt
+++ app/src/main/java/com/lovetest/MainActivity.kt
@@ -2,10 +2,18 @@ package com.lovetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    // nav 컨트롤러 정의
+    // 나중에 정의
+    lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = nav_host_fragment.findNavController()
     }
 }
